bc7a666ac3a0d3f603d2466c96069ba2ef14b00b
VaibhaV Arde <vaibhav.arde@gmail.com>
2024-10-22 17:03:18 +0530

    project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name= 'apitest',
+      version= '1.0',
+      description= 'Practice API Testing',
+      author= 'Vaibhav Arde',
+      author_email= 'vaibhav.arde@gmail.com',
+      url= 'https://www.linkedin.com/in/vaibhav-arde/',
+      packages= find_packages()
+      )
