7b21bba544da4a77023165d04adfce75c1b4f52f
VaibhaV Arde <vaibhav.arde@gmail.com>
2024-10-23 13:11:35 +0530

    Created smoke test

---
+++ apitest/tests/customers/test_create_customer_smoke.py
@@ -0,0 +1,19 @@
+import pytest
+import logging as logger
+
+
+@pytest.mark.tcid29
+def test_create_customer_only_email_password():
+    logger.info("TEST: Create new customer with email and password only.")
+    email=''
+    password=''
+    
+    # Create Payload
+    
+    # Make the call
+    
+    # verify status code of the call
+    
+    # Verify email in the response
+    
+    # Verify customer is created in database
