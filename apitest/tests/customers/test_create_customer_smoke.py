4de07e88463e545c77a66df53d43ce468fc36116
VaibhaV Arde <vaibhav.arde@gmail.com>
2024-10-24 22:05:27 +0530

    Minor update in test

--- apitest/tests/customers/test_create_customer_smoke.py
+++ apitest/tests/customers/test_create_customer_smoke.py
@@ -1,14 +1,21 @@
 import pytest
 import logging as logger
-
+from apitest.src.utilities.genericUtilities import generate_random_email_and_password
+# from '../../../apitest/src/utilities/genericUtilities.py' import generate_random_email_and_password
+# apitest/src/utilities/genericUtilities.py
 
 @pytest.mark.tcid29
 def test_create_customer_only_email_password():
     logger.info("TEST: Create new customer with email and password only.")
-    email=''
-    password=''
+    rand_info = generate_random_email_and_password()
+    email=rand_info['email']
+    password=rand_info['password']
+    print("rand_info", rand_info)
+    print("email", email)
+    print("password", password)
     
     # Create Payload
+    payload = {'email': email, 'password': password}
     
     # Make the call
     
@@ -17,3 +24,4 @@ def test_create_customer_only_email_password():
     # Verify email in the response
     
     # Verify customer is created in database
+    9209402916
