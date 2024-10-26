534f3f3914a9e1c9aaf64d36c07be2998175187a
VaibhaV Arde <vaibhav.arde@gmail.com>
2024-10-26 19:06:11 +0530

    customers helper

--- apitest/src/helpers/customers_helper.py
+++ apitest/src/helpers/customers_helper.py
@@ -1,6 +1,17 @@
 
+from apitest.src.utilities.genericUtilities import generate_random_email_and_password
+
 class CustomerHelper(object):
     def __init__(self):
         pass
     
-    def create_customer(self, email=None, )
+    def create_customer(self, email=None, password =None, ** kwargs):
+        if not email:
+            ep = generate_random_email_and_password()
+            email = ep['email']
+        if not password:
+            password = 'Password1'
+            
+        payload = dict()
+        payload['email'] = email
+        payload['password'] = password
